da3a8f9894c14163e9e7cd9030b6e6b32e4dd15e
Lorcan <72615977+lorcan2440@users.noreply.github.com>
2024-06-07 00:05:04 +0100

    Update connect_to_airpods.py

--- connect_to_airpods.py
+++ connect_to_airpods.py
@@ -18,19 +18,19 @@ i, found = 0, False
 while not found:
     try:
         subkey = EnumKey(keys, i)
-        try:
-            name = QueryValueEx(OpenKey(keys, subkey), "Name")
-            name_decoded = name[0].decode("utf-8")
-            if "AirPods" in name_decoded:
-                found = True
-                airpods_mac_addr = ":".join([subkey[j : j + 2] for j in range(0, len(subkey), 2)]).upper()
-                print(f"Found AirPods: {name_decoded}, MAC address: {airpods_mac_addr}")
-        except FileNotFoundError:
-            pass  # doesn't have a Name attribute
-        i += 1
-    except OSError:
+        name = QueryValueEx(OpenKey(keys, subkey), "Name")
+        name_decoded = name[0].decode("utf-8")
+        if "AirPods" in name_decoded:
+            found = True
+            airpods_mac_addr = ":".join([subkey[j : j + 2] for j in range(0, len(subkey), 2)]).upper()
+            print(f"Found AirPods: {name_decoded}, MAC address: {airpods_mac_addr}")
+    except FileNotFoundError:  # no Name attribute found in this key
+        pass
+    except OSError:  # end of registry keys
         print("Did not find AirPods in list of connected Bluetooth devices.")
         raise
+    finally:  # iterate to the next key
+        i += 1
 
 # Connect to the AirPods
 def connect_to_airpods(airpods_mac_addr: str):
