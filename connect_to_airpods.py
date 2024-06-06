e000617f169a0721cf9c9d0aa0ce19ea06ad2ba7
Lorcan <72615977+lorcan2440@users.noreply.github.com>
2024-06-06 23:53:40 +0100

    Update connect_to_airpods.py

--- connect_to_airpods.py
+++ connect_to_airpods.py
@@ -1,6 +1,7 @@
 import time
-import socket
-import winreg
+import threading
+from socket import socket, AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM
+from winreg import OpenKey, EnumKey, QueryValueEx, HKEY_LOCAL_MACHINE
 
 # script to be called from AutoHotKey in the startup directory
 # computer needs to be already paired with the AirPods
@@ -8,38 +9,46 @@ import winreg
 # defaults to Stereo audio
 
 
-REG_BTH_ADDRS = r'SYSTEM\CurrentControlSet\Services\BTHPORT\Parameters\Devices'
+REG_BTH_ADDRS = r"SYSTEM\CurrentControlSet\Services\BTHPORT\Parameters\Devices"
 CHANNEL = 1
 
 # Find the MAC address of the AirPods
-keys = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, REG_BTH_ADDRS)
+keys = OpenKey(HKEY_LOCAL_MACHINE, REG_BTH_ADDRS)
 i, found = 0, False
 while not found:
     try:
-        subkey = winreg.EnumKey(keys, i)
+        subkey = EnumKey(keys, i)
         try:
-            name = winreg.QueryValueEx(winreg.OpenKey(keys, subkey), 'Name')
-            if 'AirPods' in name[0].decode('utf-8'):
+            name = QueryValueEx(OpenKey(keys, subkey), "Name")
+            name_decoded = name[0].decode("utf-8")
+            if "AirPods" in name_decoded:
                 found = True
-                airpods_mac_addr = ':'.join([subkey[j:j + 2] for j in range(0, len(subkey), 2)]).upper()
-                print(f'Found AirPods with MAC address: {airpods_mac_addr}')
-                break
+                airpods_mac_addr = ":".join([subkey[j : j + 2] for j in range(0, len(subkey), 2)]).upper()
+                print(f"Found AirPods: {name_decoded}, MAC address: {airpods_mac_addr}")
         except FileNotFoundError:
             pass  # doesn't have a Name attribute
         i += 1
     except OSError:
-        print('Did not find AirPods in list of connected Bluetooth devices.')
+        print("Did not find AirPods in list of connected Bluetooth devices.")
         raise
 
 # Connect to the AirPods
-sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-try:
-    sock.connect((airpods_mac_addr, CHANNEL))  # MAC address, channel
-except OSError as e:
-    if e.errno == 10051:  # network is unreachable
-        raise
-    else:
-        print('Success - audio connection will begin within 30 seconds.')
-        time.sleep(3)
-finally:
-    sock.close()
+def connect_to_airpods(airpods_mac_addr: str):
+    sock = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM)
+    try:
+        sock.connect((airpods_mac_addr, CHANNEL))  # MAC address, channel
+    except OSError as e:
+        if e.errno == 10051:  # network is unreachable
+            raise
+        else:
+            print("Success - audio connection will begin within 30 seconds.")
+            time.sleep(3)
+    finally:
+        sock.close()
+
+
+thread = threading.Thread(target=connect_to_airpods, args=(airpods_mac_addr,), daemon=True)
+thread.start()
+thread.join(10)  # wait for 10 seconds
+if thread.is_alive():
+    print("Connection to AirPods timed out.")
